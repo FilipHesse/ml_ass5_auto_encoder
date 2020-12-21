cb29d8daa1cc63ec58de49658cbfd7c2af7e245d
Filip Hesse <Filip_Hesse@yahoo.de>
2020-12-21 23:39:41 +0100

    Add missing library for standalone usage

--- DNN/5b2_feature_extraction_VGG16.py
+++ DNN/5b2_feature_extraction_VGG16.py
@@ -1,10 +1,14 @@
 #%%
-from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 from tensorflow.keras.layers.experimental.preprocessing import Resizing
@@ -34,18 +38,23 @@ CIFAR_SHAPE = (CIFAR_SIZE, CIFAR_SIZE, CIFAR_DEPTH)
 
 
 def build_base_model(img_shape, num_classes) -> Model:
-    base_model = MobileNetV2(
+    base_model = VGG16(
         include_top=False,
         weights="imagenet",
-        input_shape=CIFAR_SHAPE
+        input_shape=CIFAR_SHAPE #(224, 224, 3)
     )
     #features = base_model.layers[-1].output
 
     num_layers = len(base_model.layers)
     print(f"Number of layers in the base model: {num_layers}")
 
-    for layer in base_model.layers:
+    for i, layer in enumerate(base_model.layers):
         layer.trainable = False
+
+
+        filters = layer.get_weights()
+        for filt in filters:
+            print(i, layer.name, filt.shape)
 
     input_img = Input(shape=img_shape)
     features = base_model(input_img)
@@ -54,8 +63,9 @@ def build_base_model(img_shape, num_classes) -> Model:
     #y_pred = Activation("softmax")(x)
 
     model = Model(
-        inputs=[input_img],
-        outputs=[features]
+        inputs=[base_model.input],
+        #outputs=[base_model.output]
+        outputs=[base_model.get_layer('block3_pool').output]
     )
 
     model.summary()
@@ -71,8 +81,19 @@ def build_my_model(feature_shape, num_classes) -> Model:
         layer.trainable = False
 
     input_features = Input(shape=feature_shape)
-    x = GlobalAveragePooling2D()(input_features)
-    x = Dense(units=100)(x)
+    #x = MaxPooling2D()(input_features)
+    x = Conv2D(512, 3, padding="same")(input_features)
+    x = Activation("relu")(x)
+    x = MaxPooling2D()(x)
+    x = Conv2D(1024, 3, padding="same")(input_features)
+    x = Activation("relu")(x)
+    x = MaxPooling2D()(x)
+    x = Flatten()(x)
+    x = Dense(units=512)(x)
+    x = Activation("relu")(x)
+    x = Dense(units=256)(x)
+    x = Activation("relu")(x)
+    x = Dense(units=128)(x)
     x = Activation("relu")(x)
     x = Dense(units=num_classes)(x)
     y_pred = Activation("softmax")(x)
@@ -94,9 +115,14 @@ if __name__ == "__main__":
     (val_x, val_y) = data.get_val_set()
     (test_x, test_y) = data.get_test_set()
 
+    plt.imsave(f"output/feature_extraction/test_image_0.jpg", train_x[0])
+
     img_shape = data.img_shape
     num_classes = data.num_classes
 
+    train_x = preprocess_input(train_x)
+    val_x = preprocess_input(val_x)
+    test_x = preprocess_input(test_x)
 
     # Global params
     epochs = 100
@@ -121,22 +147,22 @@ if __name__ == "__main__":
         restore_best_weights=True
     )
 
-    # base_model.fit(
-    #     train_x,
-    #     train_y,
-    #     verbose=1,
-    #     epochs=epochs,
-    #     callbacks=[es_callback],
-    #     validation_data=(val_x, val_y),
-    #     batch_size=128
-    # )
-
-
-
-    
-    # take a subset of trainingset, because GPU cant handle entire trainingset
     features_train = base_model.predict(train_x, batch_size=32)
     features_val = base_model.predict(val_x, batch_size=32)
+
+#%%
+    square = 8
+    i=1
+    fig = plt.figure()
+    for _ in range(square):
+        for _ in range(square):
+            ax = fig.add_subplot(square, square, i)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.imshow(features_train[0,:,:,i-1],cmap='gray')
+            i += 1
+
+    fig.savefig(f"output/feature_extraction/test_image_0_features.jpg")
 
     my_model = build_my_model(
         features_train.shape[1:],
@@ -167,20 +193,5 @@ if __name__ == "__main__":
     )
 
     print(f"Scores: {scores}")
-    # for i, pred in enumerate(preds):
-    #     if i > nr_output_predictions :
-    #         break
-    #     pred_label, pred_probability = data.decode_labels(pred)
-    #     y_label, y_probability = data.decode_labels(test_y[i])
-    #     #plt.imshow(test_x[i])
-    #     plt.imsave(f"output/transfer_learning/{i}_{pred_label}.jpg", test_x[i])
-        
-    #     print(f"Predicted class: {pred_label} ({pred_probability*100}%), Actual class: {y_label}")
-    
-    # model.save_weights(filepath=MODEL_FILE_PATH)
-
-    # model.load_weights(filepath=MODEL_FILE_PATH)
-
-
 
 # %%
---
+++ DNN/cifarData.py
@@ -0,0 +1,63 @@
+from typing import Tuple
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
+from tensorflow.image import resize
+
+
+IMAGENET_SHAPE = [96, 96]
+
+
+class CIFAR10:
+    def __init__(self, validation_size: float = 0.33) -> None:
+        np.random.seed(1)
+        tf.random.set_seed(1)
+        # User-definen constants
+        self.num_classes = 10
+        # Load the data set
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        # Split the dataset, only consider 10000 values
+        x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=validation_size)
+        # Preprocess x data
+        self.x_train = x_train.astype(np.float32)
+        self.x_test = x_test.astype(np.float32)
+        self.x_val = x_val.astype(np.float32)
+
+        self.x_train = (self.x_train / 255.0)
+        self.x_test = (self.x_test / 255.0)
+        self.x_val = (self.x_val / 255.0)
+        # Preprocess y data
+        self.y_train = to_categorical(y_train, num_classes=self.num_classes)
+        self.y_test = to_categorical(y_test, num_classes=self.num_classes)
+        self.y_val = to_categorical(y_val, num_classes=self.num_classes)
+        # Dataset attributes
+        self.train_size = self.x_train.shape[0]
+        self.test_size = self.x_test.shape[0]
+        self.val_size = self.x_val.shape[0]
+        self.width = self.x_train.shape[1]
+        self.height = self.x_train.shape[2]
+        self.depth = self.x_train.shape[3]
+        self.img_shape = (self.width, self.height, self.depth)
+
+    def get_train_set(self) -> Tuple[np.ndarray, np.ndarray]:
+        return self.x_train, self.y_train
+
+    def get_test_set(self) -> Tuple[np.ndarray, np.ndarray]:
+        return self.x_test, self.y_test
+
+    def get_val_set(self) -> Tuple[np.ndarray, np.ndarray]:
+        return self.x_val, self.y_val
+
+    def load_label_names(self):
+        return np.array(['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'])
+
+    def decode_labels(self, y_numeric):
+        max_probability = np.amax(y_numeric)
+        max_label = self.load_label_names()[y_numeric==max_probability][0]
+        return max_label, max_probability
+
+
